bea934096fcafa7e9b93c5e413f2648533cf8594
IuriAlex <143138886+IuriAlex@users.noreply.github.com>
2023-11-02 17:17:16 -0300

    Add files via upload

---
+++ ProjetoPOOII/Fastapi/App/dependencias/dep.py
@@ -0,0 +1,14 @@
+from typing import Generator
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from App.dependencias.database import Session
+
+
+async def get_session() -> Generator:
+    session: AsyncSession = Session()
+
+    try:  # Abre conexão
+        yield session
+    finally:  # Fecha conexão
+        await session.close()
---
+++ ProjetoPOOII/Fastapi/README.md
@@ -0,0 +1,1 @@
+# AvaliacaoPoo2
